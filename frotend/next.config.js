bc7296bb5755ddfc3d017ce50fc738f88f82e649
Arsi Richter <106626808+ARILDMT@users.noreply.github.com>
2025-02-25 09:29:34 +0000

    q1

---
+++ frotend/next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    reactStrictMode: true,
+    // Настройки интернационализации (i18n)
+    i18n: {
+      locales: ['en', 'ru', 'es', 'fr'],
+      defaultLocale: 'en'
+    }
+  };
